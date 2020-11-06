1308d9bbc6910805d220e7c005a5b9f0fbe2349c
quentin roux <quentin.roux@hotmail.fr>
2020-11-06 21:03:06 +0100

    hooks working

--- life-cycles-interview/components/List.js
+++ life-cycles-interview/components/List.js
@@ -1,50 +1,114 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 
-
-class List extends Component {
-  state = {
-    users: []
-  }
-
-  async componentDidMount() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const toJson = await response.json()
-
-    setTimeout(() => {
-      this.setState({users: toJson})
-    }, 3000)
-
-
+export default function Hello() {
+	const [users, setUsers] = useState([])
+  
+  useEffect(() => {  
+  	(async () => {
+    	const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    	const users = await response.json()   
+     
+      setUsers(users) 
+    })()
     
-  }
 
-  renderUsers = () => {
-		const users = this.state.users;
-    const listItems = users.map((user) =>	{
+  
+    console.log(users) 	
+  }, [])
+
+  const renderUsers = () => {
+    return users.map(user => {
       return (
-        <div key={user.name}>
-          <h4 >{user.name}</h4>
-          <p>{user.id} - {user.address.city} </p>
-        </div> 
+        <h4>{user.id} - {user.name}</h4>  
       )
     })
-
-    console.log(listItems)
-    return listItems
   }
 
-
-  render() {
-    return (
-      <div className="container">
-        <h1>List of Users</h1>
-        {this.state.users.length}
-        {this.renderUsers()}
-
-      </div> 
+  return (
+    <div className="container">
+      <h1>List of Users</h1>
+      {users.length}
+      {renderUsers(users)}
+    </div> 
     )
-  }
+
 }
 
 
-export default List;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+
+
+// class List extends Component {
+//   state = {
+//     users: []
+//   }
+
+//   async componentDidMount() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const toJson = await response.json()
+
+//     setTimeout(() => {
+//       this.setState({users: toJson})
+//     }, 3000)
+
+
+    
+//   }
+
+//   renderUsers = () => {
+// 		const users = this.state.users;
+//     const listItems = users.map((user) =>	{
+//       return (
+//         <div key={user.name}>
+//           <h4 >{user.name}</h4>
+//           <p>{user.id} - {user.address.city} </p>
+//         </div> 
+//       )
+//     })
+
+//     console.log(listItems)
+//     return listItems
+//   }
+
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <h1>List of Users</h1>
+//         {this.state.users.length}
+//         {this.renderUsers()}
+
+//       </div> 
+//     )
+//   }
+// }
+
+
+// export default List;
